04457bc2cc78db4f49914d3166e7b91114a5114f
tanayyyyy7 <tanayyyyy7@gmail.com>
2025-05-02 15:55:00 +0000

    enabled cors for all origins

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import cartRoutes from "./routes/cart.routes";
 import orderRoutes from "./routes/order.routes";
 import webhookRoutes from "./routes/webhook.routes"; // Import webhook routes
 import { authenticateToken } from "./middleware/auth.middleware";
+import cors from "cors";
 
 const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
@@ -13,6 +14,7 @@ const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 // Mount webhook routes BEFORE express.json()
 // app.use('/api/webhooks', webhookRoutes);
 
+app.use(cors()); // Enable CORS for all origins
 app.use(express.json());
 
 app.get('/protected', authenticateToken, (req, res) => {
